7a3aea017adef635e8dbf4c2983eb3000b93030a
vilkku89 <36002316+vilkku89@users.noreply.github.com>
2019-08-31 17:40:32 +0300

    Create map5.js

---
+++ map5.js
@@ -0,0 +1,219 @@
+                var bounds = null;
+            
+
+            var map_1601690360284e87b97332dcbc9aaada = L.map(
+                                  'map_1601690360284e87b97332dcbc9aaada',
+                                  {center: [0,0],
+                                  zoom: 1,
+                                  maxBounds: bounds,
+                                  layers: [],
+                                  worldCopyJump: false,
+                                  crs: L.CRS.EPSG3857
+                                 });
+            
+        
+    
+            var tile_layer_c73f7ddd5ad54b28b5fcf25e9d6c918e = L.tileLayer(
+                'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
+                {
+  "attribution": null,
+  "detectRetina": false,
+  "maxNativeZoom": 18,
+  "maxZoom": 18,
+  "minZoom": 2,
+  "noWrap": false,
+  "subdomains": "abc"
+}
+                ).addTo(map_1601690360284e87b97332dcbc9aaada);
+        
+    
+            var feature_group_401747048ee3451f81347bf4ac4a8ff1 = L.featureGroup(
+                ).addTo(map_1601690360284e87b97332dcbc9aaada);
+        
+    
+            var circle_marker_a663e287b735483d8d4ce1c50f62e915 = L.circleMarker(
+                [-34.479999005399996, -57.8400024734],
+                {
+  "bubblingMouseEvents": true,
+  "color": "#A0A0A0",
+  "dashArray": null,
+  "dashOffset": null,
+  "fill": false,
+  "fillColor": "#A0A0A0",
+  "fillOpacity": 0.2,
+  "fillRule": "evenodd",
+  "lineCap": "round",
+  "lineJoin": "round",
+  "opacity": 1.0,
+  "radius": 1,
+  "stroke": true,
+  "weight": 3
+}
+                )
+                
+                .addTo(feature_group_401747048ee3451f81347bf4ac4a8ff1);
+            
+    
+            var popup_bd119470fa6a42edb1f97fa18eefe12a = L.popup({maxWidth: '300'});
+
+            
+                var html_4b46be8d283741d498aab38077aa2bba = $('<div id="html_4b46be8d283741d498aab38077aa2bba" style="width: 100.0%; height: 100.0%;">Colonia del Sacramento</div>')[0];
+                popup_bd119470fa6a42edb1f97fa18eefe12a.setContent(html_4b46be8d283741d498aab38077aa2bba);
+            
+
+            circle_marker_a663e287b735483d8d4ce1c50f62e915.bindPopup(popup_bd119470fa6a42edb1f97fa18eefe12a);
+
+            
+        
+    
+            var circle_marker_630f86a583534bf0b25e71ca237a5573 = L.circleMarker(
+                [-33.5439989374, -56.90099656020001],
+                {
+  "bubblingMouseEvents": true,
+  "color": "#A0A0A0",
+  "dashArray": null,
+  "dashOffset": null,
+  "fill": false,
+  "fillColor": "#A0A0A0",
+  "fillOpacity": 0.2,
+  "fillRule": "evenodd",
+  "lineCap": "round",
+  "lineJoin": "round",
+  "opacity": 1.0,
+  "radius": 1,
+  "stroke": true,
+  "weight": 3
+}
+                )
+                
+                .addTo(feature_group_401747048ee3451f81347bf4ac4a8ff1);
+            
+    
+            var popup_f1a5b45465734d2f8846064df5ce2f3e = L.popup({maxWidth: '300'});
+
+            
+                var html_03c899bae73a43ea8be1a76fd8af1e62 = $('<div id="html_03c899bae73a43ea8be1a76fd8af1e62" style="width: 100.0%; height: 100.0%;">Trinidad</div>')[0];
+                popup_f1a5b45465734d2f8846064df5ce2f3e.setContent(html_03c899bae73a43ea8be1a76fd8af1e62);
+            
+
+            circle_marker_630f86a583534bf0b25e71ca237a5573.bindPopup(popup_f1a5b45465734d2f8846064df5ce2f3e);
+
+            
+        
+    
+            var circle_marker_3f7d4368dc96439b94e64b5ff1538105 = L.circleMarker(
+                [-33.1389990288, -58.3039974719],
+                {
+  "bubblingMouseEvents": true,
+  "color": "#A0A0A0",
+  "dashArray": null,
+  "dashOffset": null,
+  "fill": false,
+  "fillColor": "#A0A0A0",
+  "fillOpacity": 0.2,
+  "fillRule": "evenodd",
+  "lineCap": "round",
+  "lineJoin": "round",
+  "opacity": 1.0,
+  "radius": 1,
+  "stroke": true,
+  "weight": 3
+}
+                )
+                
+                .addTo(feature_group_401747048ee3451f81347bf4ac4a8ff1);
+            
+    
+            var popup_889fb275096a40d3843331a2bd28fed5 = L.popup({maxWidth: '300'});
+
+            
+                var html_98ee17eb7e154e40bb39d67beb17abeb = $('<div id="html_98ee17eb7e154e40bb39d67beb17abeb" style="width: 100.0%; height: 100.0%;">Fray Bentos</div>')[0];
+                popup_889fb275096a40d3843331a2bd28fed5.setContent(html_98ee17eb7e154e40bb39d67beb17abeb);
+            
+
+            circle_marker_3f7d4368dc96439b94e64b5ff1538105.bindPopup(popup_889fb275096a40d3843331a2bd28fed5);
+
+            
+        
+    
+            var circle_marker_ee701d5364254a4f87ef9e7ffa48260a = L.circleMarker(
+                [-34.5380040067, -56.284001493199995],
+                {
+  "bubblingMouseEvents": true,
+  "color": "#A0A0A0",
+  "dashArray": null,
+  "dashOffset": null,
+  "fill": false,
+  "fillColor": "#A0A0A0",
+  "fillOpacity": 0.2,
+  "fillRule": "evenodd",
+  "lineCap": "round",
+  "lineJoin": "round",
+  "opacity": 1.0,
+  "radius": 1,
+  "stroke": true,
+  "weight": 3
+}
+                )
+                
+                .addTo(feature_group_401747048ee3451f81347bf4ac4a8ff1);
+            
+    
+            var popup_f32decad5c624a12b163b1a81f58e9f0 = L.popup({maxWidth: '300'});
+
+            
+                var html_6db68a21bddd445ab9ef0dd121e4959b = $('<div id="html_6db68a21bddd445ab9ef0dd121e4959b" style="width: 100.0%; height: 100.0%;">Canelones</div>')[0];
+                popup_f32decad5c624a12b163b1a81f58e9f0.setContent(html_6db68a21bddd445ab9ef0dd121e4959b);
+            
+
+            circle_marker_ee701d5364254a4f87ef9e7ffa48260a.bindPopup(popup_f32decad5c624a12b163b1a81f58e9f0);
+
+            
+        
+    
+            var circle_marker_ccadfab96b4648cc8a8ebca6c6c7ecee = L.circleMarker(
+                [-34.0990020052, -56.214998448],
+                {
+  "bubblingMouseEvents": true,
+  "color": "#A0A0A0",
+  "dashArray": null,
+  "dashOffset": null,
+  "fill": false,
+  "fillColor": "#A0A0A0",
+  "fillOpacity": 0.2,
+  "fillRule": "evenodd",
+  "lineCap": "round",
+  "lineJoin": "round",
+  "opacity": 1.0,
+  "radius": 1,
+  "stroke": true,
+  "weight": 3
+}
+                )
+                
+                .addTo(feature_group_401747048ee3451f81347bf4ac4a8ff1);
+            
+    
+            var popup_7bce3463c4a64776a0a6ff2a2ae6d654 = L.popup({maxWidth: '300'});
+
+            
+                var html_cd5167de1fa14c089de9803fe8042086 = $('<div id="html_cd5167de1fa14c089de9803fe8042086" style="width: 100.0%; height: 100.0%;">Florida</div>')[0];
+                popup_7bce3463c4a64776a0a6ff2a2ae6d654.setContent(html_cd5167de1fa14c089de9803fe8042086);
+            
+
+            circle_marker_ccadfab96b4648cc8a8ebca6c6c7ecee.bindPopup(popup_7bce3463c4a64776a0a6ff2a2ae6d654);
+
+            
+        
+    
+            var layer_control_95cbba289a0943a9b009029ae4a23d13 = {
+                base_layers : { "cartodbdark_matter" : tile_layer_c73f7ddd5ad54b28b5fcf25e9d6c918e, },
+                overlays : { "Cities" : feature_group_401747048ee3451f81347bf4ac4a8ff1, }
+                };
+            L.control.layers(
+                layer_control_95cbba289a0943a9b009029ae4a23d13.base_layers,
+                layer_control_95cbba289a0943a9b009029ae4a23d13.overlays,
+                {position: 'topright',
+                 collapsed: true,
+                 autoZIndex: true
+                }).addTo(map_1601690360284e87b97332dcbc9aaada);
